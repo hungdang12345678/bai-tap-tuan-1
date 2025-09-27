200c271b2fa36245cde1eaa2645e634b532186aa
hungdang12345678 <hungdangqh27@gmail.com>
2025-09-27 14:24:58 +0700

    Add files via upload

---
+++ bai4tttl/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace bai4tttl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSo1 = new System.Windows.Forms.TextBox();
+            this.txtSo2 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radChia = new System.Windows.Forms.RadioButton();
+            this.radNhan = new System.Windows.Forms.RadioButton();
+            this.radTru = new System.Windows.Forms.RadioButton();
+            this.radCong = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtKQ = new System.Windows.Forms.TextBox();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(160, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Số 1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(160, 160);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Số 2";
+            // 
+            // txtSo1
+            // 
+            this.txtSo1.Location = new System.Drawing.Point(236, 107);
+            this.txtSo1.Name = "txtSo1";
+            this.txtSo1.Size = new System.Drawing.Size(307, 22);
+            this.txtSo1.TabIndex = 2;
+            this.txtSo1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtSo2
+            // 
+            this.txtSo2.Location = new System.Drawing.Point(236, 160);
+            this.txtSo2.Name = "txtSo2";
+            this.txtSo2.Size = new System.Drawing.Size(307, 22);
+            this.txtSo2.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radChia);
+            this.groupBox1.Controls.Add(this.radNhan);
+            this.groupBox1.Controls.Add(this.radTru);
+            this.groupBox1.Controls.Add(this.radCong);
+            this.groupBox1.Location = new System.Drawing.Point(163, 210);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(380, 86);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Phép tính";
+            // 
+            // radChia
+            // 
+            this.radChia.AutoSize = true;
+            this.radChia.Location = new System.Drawing.Point(286, 38);
+            this.radChia.Name = "radChia";
+            this.radChia.Size = new System.Drawing.Size(55, 20);
+            this.radChia.TabIndex = 3;
+            this.radChia.TabStop = true;
+            this.radChia.Text = "Chia";
+            this.radChia.UseVisualStyleBackColor = true;
+            this.radChia.CheckedChanged += new System.EventHandler(this.radChia_CheckedChanged);
+            // 
+            // radNhan
+            // 
+            this.radNhan.AutoSize = true;
+            this.radNhan.Location = new System.Drawing.Point(184, 38);
+            this.radNhan.Name = "radNhan";
+            this.radNhan.Size = new System.Drawing.Size(63, 20);
+            this.radNhan.TabIndex = 2;
+            this.radNhan.TabStop = true;
+            this.radNhan.Text = "Nhân ";
+            this.radNhan.UseVisualStyleBackColor = true;
+            this.radNhan.CheckedChanged += new System.EventHandler(this.radNhan_CheckedChanged);
+            // 
+            // radTru
+            // 
+            this.radTru.AutoSize = true;
+            this.radTru.Location = new System.Drawing.Point(100, 38);
+            this.radTru.Name = "radTru";
+            this.radTru.Size = new System.Drawing.Size(48, 20);
+            this.radTru.TabIndex = 1;
+            this.radTru.TabStop = true;
+            this.radTru.Text = "Trừ";
+            this.radTru.UseVisualStyleBackColor = true;
+            this.radTru.CheckedChanged += new System.EventHandler(this.radTru_CheckedChanged);
+            // 
+            // radCong
+            // 
+            this.radCong.AutoSize = true;
+            this.radCong.Location = new System.Drawing.Point(6, 38);
+            this.radCong.Name = "radCong";
+            this.radCong.Size = new System.Drawing.Size(60, 20);
+            this.radCong.TabIndex = 0;
+            this.radCong.TabStop = true;
+            this.radCong.Text = "Cộng";
+            this.radCong.UseVisualStyleBackColor = true;
+            this.radCong.CheckedChanged += new System.EventHandler(this.radCong_CheckedChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(160, 322);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Kết quả";
+            // 
+            // txtKQ
+            // 
+            this.txtKQ.Location = new System.Drawing.Point(236, 322);
+            this.txtKQ.Name = "txtKQ";
+            this.txtKQ.Size = new System.Drawing.Size(307, 22);
+            this.txtKQ.TabIndex = 6;
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(154, 372);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(75, 33);
+            this.btnThoat.TabIndex = 7;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.txtKQ);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtSo2);
+            this.Controls.Add(this.txtSo1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSo1;
+        private System.Windows.Forms.TextBox txtSo2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radChia;
+        private System.Windows.Forms.RadioButton radNhan;
+        private System.Windows.Forms.RadioButton radTru;
+        private System.Windows.Forms.RadioButton radCong;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtKQ;
+        private System.Windows.Forms.Button btnThoat;
+    }
+}
+
---
+++ bai4tttl/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+
+namespace bai4tttl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private bool KiemTraNhap(out double a, out double b)
+        {
+            a = 0; b = 0;
+
+            if (!double.TryParse(txtSo1.Text.Trim(), out a))
+            {
+                MessageBox.Show("S? 1 không h?p l?!", "L?i",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtSo1.Focus();
+                return false;
+            }
+
+            if (!double.TryParse(txtSo2.Text.Trim(), out b))
+            {
+                MessageBox.Show("S? 2 không h?p l?!", "L?i",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtSo2.Focus();
+                return false;
+            }
+
+            return true;
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radCong_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radCong.Checked && KiemTraNhap(out double a, out double b))
+                txtKQ.Text = (a + b).ToString();
+        }
+
+        private void radNhan_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radNhan.Checked && KiemTraNhap(out double a, out double b))
+                txtKQ.Text = (a * b).ToString();
+        }
+
+        private void radTru_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radTru.Checked && KiemTraNhap(out double a, out double b))
+                txtKQ.Text = (a - b).ToString();
+        }
+
+        private void radChia_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radChia.Checked && KiemTraNhap(out double a, out double b))
+            {
+                if (b == 0)
+                {
+                    MessageBox.Show("Không th? chia cho 0!", "L?i",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    txtSo2.Focus();
+                    return;
+                }
+                txtKQ.Text = (a / b).ToString();
+            }
+        }
+        private void FrmBai4_Load(object sender, EventArgs e)
+        {
+            txtSo1.Clear();
+            txtSo2.Clear();
+            txtKQ.Clear();
+            radCong.Checked = false;
+            radTru.Checked = false;
+            radNhan.Checked = false;
+            radChia.Checked = false;
+            txtSo1.Focus();
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            Application.Exit();
+        }
+    }
+}
+
