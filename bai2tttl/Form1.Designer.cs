80924170536a285d1cdfefd6da098eaffbde05b8
hungdang12345678 <hungdangqh27@gmail.com>
2025-09-27 13:31:07 +0700

    Add files via upload

---
+++ bai2tttl/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace bai2tttl
+{
+    partial class FrmBai2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtHoTen = new System.Windows.Forms.TextBox();
+            this.rad1 = new System.Windows.Forms.RadioButton();
+            this.rad2 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtKQ = new System.Windows.Forms.TextBox();
+            this.btnKQ = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnDung = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(160, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nhập họ và tên:";
+            // 
+            // txtHoTen
+            // 
+            this.txtHoTen.Location = new System.Drawing.Point(277, 101);
+            this.txtHoTen.Name = "txtHoTen";
+            this.txtHoTen.Size = new System.Drawing.Size(364, 22);
+            this.txtHoTen.TabIndex = 3;
+            // 
+            // rad1
+            // 
+            this.rad1.AutoSize = true;
+            this.rad1.Location = new System.Drawing.Point(6, 33);
+            this.rad1.Name = "rad1";
+            this.rad1.Size = new System.Drawing.Size(92, 20);
+            this.rad1.TabIndex = 4;
+            this.rad1.TabStop = true;
+            this.rad1.Text = "chữ thường";
+            this.rad1.UseVisualStyleBackColor = true;
+            this.rad1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // rad2
+            // 
+            this.rad2.AutoSize = true;
+            this.rad2.Location = new System.Drawing.Point(6, 74);
+            this.rad2.Name = "rad2";
+            this.rad2.Size = new System.Drawing.Size(105, 20);
+            this.rad2.TabIndex = 5;
+            this.rad2.TabStop = true;
+            this.rad2.Text = "CHỮ IN HOA";
+            this.rad2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rad1);
+            this.groupBox1.Controls.Add(this.rad2);
+            this.groupBox1.Location = new System.Drawing.Point(157, 146);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(420, 122);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Chọn kiểu chữ";
+            // 
+            // txtKQ
+            // 
+            this.txtKQ.Location = new System.Drawing.Point(277, 275);
+            this.txtKQ.Name = "txtKQ";
+            this.txtKQ.Size = new System.Drawing.Size(364, 22);
+            this.txtKQ.TabIndex = 7;
+            // 
+            // btnKQ
+            // 
+            this.btnKQ.Location = new System.Drawing.Point(148, 274);
+            this.btnKQ.Name = "btnKQ";
+            this.btnKQ.Size = new System.Drawing.Size(107, 28);
+            this.btnKQ.TabIndex = 8;
+            this.btnKQ.Text = "Kết quả";
+            this.btnKQ.UseVisualStyleBackColor = true;
+            this.btnKQ.Click += new System.EventHandler(this.btnKQ_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(583, 156);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(58, 112);
+            this.btnXoa.TabIndex = 9;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnDung
+            // 
+            this.btnDung.Location = new System.Drawing.Point(148, 323);
+            this.btnDung.Name = "btnDung";
+            this.btnDung.Size = new System.Drawing.Size(75, 23);
+            this.btnDung.TabIndex = 10;
+            this.btnDung.Text = "Dừng";
+            this.btnDung.UseVisualStyleBackColor = true;
+            this.btnDung.Click += new System.EventHandler(this.btnDung_Click);
+            // 
+            // FrmBai2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDung);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnKQ);
+            this.Controls.Add(this.txtKQ);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtHoTen);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmBai2";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtHoTen;
+        private System.Windows.Forms.RadioButton rad1;
+        private System.Windows.Forms.RadioButton rad2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtKQ;
+        private System.Windows.Forms.Button btnKQ;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnDung;
+    }
+}
+
---
+++ bai2tttl/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bai2tttl
+{
+    public partial class FrmBai2 : Form
+    {
+        public FrmBai2()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKQ_Click(object sender, EventArgs e)
+        {
+            string hoten = this.txtHoTen.Text.Trim();
+
+            if (this.rad1.Checked == true)   // chữ thường
+                this.txtKQ.Text = hoten.ToLower();
+
+            if (this.rad2.Checked == true)   // chữ in hoa
+                this.txtKQ.Text = hoten.ToUpper();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            this.txtHoTen.Clear();
+            this.txtKQ.Clear();
+            this.rad1.Checked = true;   // 
+            this.txtHoTen.Focus();      // 
+        }
+
+        private void btnDung_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            Application.Exit();
+        }
+    }
+    }
+
